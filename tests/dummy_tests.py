99b8d041ffb271489e48d9d8265968de7176b245
JoshuaKouri <jkouri@ipgphotonics.com>
2024-06-17 11:08:41 -0400

    Created basic folder structure

---
+++ tests/dummy_tests.py
@@ -0,0 +1,15 @@
+"""
+Dummy test.
+
+TODO: Delete this file.
+"""
+
+import pytest
+
+def test_passing():
+    # Passing test.
+    assert 2 > 1
+
+def test_failing():
+    # Failing test.
+    assert 1 > 2
